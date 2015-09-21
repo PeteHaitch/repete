1dc5cfea6668a93eb0fd726ac0c455d0e1d412c0
Peter Hickey <peter.hickey@gmail.com>
2015-09-21 19:30:57 +1000

    Import all non-base functions

---
+++ R/repete.R
@@ -0,0 +1,8 @@
+#' Peter Hickey's R Code.
+#'
+#' Miscellaneous R functions that I find useful.
+#'
+#' @docType package
+#' @name repete-package
+#' @importFrom utils capture.output object.size
+NULL
