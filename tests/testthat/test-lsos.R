75819f1bcab817d91d43fc60157383cd10804d02
Peter Hickey <peter.hickey@gmail.com>
2015-09-21 19:59:20 +1000

    Uncomment troublesome test.

--- tests/testthat/test-lsos.R
+++ tests/testthat/test-lsos.R
@@ -9,13 +9,15 @@ test_that("lsos() works", {
   env$x <- matrix(1:100, ncol = 4)
   env$y <- data.frame(a = letters[1:10], b = 10:1)
   env$z <- seq_len(10)
+  # TODO: Remove once test fixed on windows (see
+  # https://github.com/PeteHaitch/repete/issues/1)
   print(lsos(env))
-#   expect_identical(lsos(env),
-#                    data.frame(Name = c("y", "x", "z"),
-#                               Type = c("data.frame", "matrix", "integer"),
-#                               Size = c(1840, 600, 88),
-#                               PrettySize = c("1.8 Kb", "600 bytes", "88 bytes"),
-#                               Rows = c(10, 25, 10),
-#                               Columns = c(2, 4, NA),
-#                               stringsAsFactors = FALSE))
+  expect_identical(lsos(env),
+                   data.frame(Name = c("y", "x", "z"),
+                              Type = c("data.frame", "matrix", "integer"),
+                              Size = c(1840, 600, 88),
+                              PrettySize = c("1.8 Kb", "600 bytes", "88 bytes"),
+                              Rows = c(10, 25, 10),
+                              Columns = c(2, 4, NA),
+                              stringsAsFactors = FALSE))
 })
